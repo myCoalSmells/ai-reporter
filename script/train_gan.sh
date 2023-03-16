9333621dc5bc3c63f2933cee2ddf63188c81d386
Andrew Bai <andrewcybai@gmail.com>
2023-03-16 13:45:52 -0700

    Update train_gan.sh

--- script/train_gan.sh
+++ script/train_gan.sh
@@ -5,8 +5,8 @@ set -e
 
 SRC_DIR=$1 # "/home/andrewbai/data/cell_img2img/${TGT}_MSC_20x_${SRC}/input"
 TGT_DIR=$2 # "/home/andrewbai/data/cell_img2img/${TGT}_MSC_20x_${SRC}/output"
-LOAD_UNET_PATH=$3 # "./checkpoints/baseline_CD29_PC.ckpt"
-LOG_DIR=$4 # "./gan_logs/${SRC}_${TGT}"
+LOG_DIR=$3 # "./gan_logs/${SRC}_${TGT}"
+LOAD_UNET_PATH=$4 # "./checkpoints/baseline_CD29_PC.ckpt"
 KWARGS=$5 # --max_epochs 200 --save_every_n_epochs 20, --max_B_size 20, --name ntrn-100
 
 mkdir -p $LOG_DIR
